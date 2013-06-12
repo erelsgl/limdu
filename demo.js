13e6148956601517b0ff5f4cf145787a34888d8a
erelsgl <erelsgl@gmail.com>
2013-06-12 13:58:56 +0300

    fix demo

--- demo.js
+++ demo.js
@@ -1,36 +1,11 @@
 console.log("Hello world!");
 
+var classifier = require('./classifier');
 
-/* Perceptron demo: */
+var bayes = new classifier.Bayesian();
 
-var perceptron = require('perceptron/perceptron')
+bayes.train("cheap replica watches", 'spam');
+bayes.train("I don't know if this works on windows", 'not');
 
-var and = perceptron()
-
-and.train([1, 1], 1)
-and.train([0, 1], 0)
-and.train([1, 0], 0)
-and.train([0, 0], 0)
-
-// practice makes perfect (we hope...)
-while(!and.retrain()) {}
-
-console.log("2 FEATURES: ");
-console.log(and.perceive([1, 1])) // 1
-console.log(and.perceive([0, 1])) // 0
-console.log(and.perceive([1, 0])) // 0
-console.log(and.perceive([0, 0])) // 0
-console.log(and.perceive([2, 0])) // ?
-console.log(and.perceive([0, 2])) // ?
-
-console.log("3 FEATURES: ");
-console.log(and.perceive([0, 0, 0])) // 0
-console.log(and.perceive([0, 0, 1])) // 0
-console.log(and.perceive([0, 1, 0])) // 0
-console.log(and.perceive([1, 0, 0])) // 0
-console.log(and.perceive([1, 1, 0])) // 0
-console.log(and.perceive([1, 0, 1])) // 0
-console.log(and.perceive([0, 1, 1])) // 0
-console.log(and.perceive([1, 1, 1])) // 0
-
-console.dir(and);
+var category = bayes.classify("free watches"); 
+console.log(category);  // "spam"

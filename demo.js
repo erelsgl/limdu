97f0c9df932b93a22c0a5884a80c46ac3bc79258
erelsgl <erelsgl@gmail.com>
2013-06-12 22:47:52 +0300

    minor demo modification

--- demo.js
+++ demo.js
@@ -6,11 +6,12 @@ var BinaryClassifierSet = require('./BinaryClassifierSet');
 
 var bcs = new BinaryClassifierSet(classifier.Bayesian, {}, {});
 
-bcs.addClasses(['spam', 'clocks', 'windows', 'important'])
+bcs.addClasses(['spam', 'clocks', 'windows', 'important', 'pills'])
 bcs.train("cheap replica watch es", ['spam', 'clocks']);
 bcs.train("your watch is ready", ['clocks', 'important']);
 bcs.train("I don't know if this works on windows", ['windows', 'important']);
 bcs.train("cheap windows !!!", ['windows', 'spam']);
+bcs.train("get this for cheap !!!", ['spam']);
 
 //console.log(JSON.stringify(bcs.toJSON()));
 

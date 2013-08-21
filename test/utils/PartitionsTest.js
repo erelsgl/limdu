640d107f3064e81ec150feb9a0f35ba505362b26
erelsgl <erelsgl@gmail.com>
2013-08-21 12:16:32 +0300

    add PartitionsTest

---
+++ test/utils/PartitionsTest.js
@@ -0,0 +1,37 @@
+/**
+ * a unit-test for Multi-Label classification
+ * 
+ * @author Erel Segal-Halevi
+ * @since 2013-08
+ */
+
+var should = require('should');
+var mlutils = require('../../utils');
+var _ = require('underscore');
+
+describe('partitions', function() {
+	// A dummy dataset with 10 documents:
+	var dataset = [1,2,3,4,5,6,7,8,9,10];
+	it('should create 5 partitions, with a test-set of 2 in each', function() {
+		var numOfPartitions = 0;
+		mlutils.partitions.partitions(dataset, 5, function(train, test, index) {
+			//console.log("\t"+index+": "+train+" / "+test);
+			train.should.have.lengthOf(8);
+			test.should.have.lengthOf(2);
+			_(test).intersection(train).should.have.lengthOf(0); // most important test - make sure there is no leak from train to test!
+			numOfPartitions++;
+		});
+		numOfPartitions.should.equal(5);
+	});
+	it('should create 3 partitions, with a test-set of 3 in each', function() {
+		var numOfPartitions = 0;
+		mlutils.partitions.partitions(dataset, 3, function(train, test, index) {
+			//console.log("\t"+index+": "+train+" / "+test);	
+			train.should.have.lengthOf(7);
+			test.should.have.lengthOf(3);
+			_(test).intersection(train).should.have.lengthOf(0); // most important test - make sure there is no leak from train to test!
+			numOfPartitions++;
+		});
+		numOfPartitions.should.equal(3);
+	});
+})

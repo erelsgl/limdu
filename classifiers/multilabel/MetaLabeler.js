6d6f99e5650e757fa3a4870aa2a7681d7bb0f541
erelsgl <erelsgl@gmail.com>
2013-11-27 13:45:32 +0200

    add doc to metalabeler

--- classifiers/multilabel/MetaLabeler.js
+++ classifiers/multilabel/MetaLabeler.js
@@ -8,6 +8,13 @@ var _ = require("underscore")._;
  * Tang Lei, Rajan Suju, Narayanan Vijay K.. Large scale multi-label classification via metalabeler in Proceedings of the 18th international conference on World wide webWWW '09(New York, NY, USA):211-220ACM 2009.
  * http://www.citeulike.org/user/erelsegal-halevi/article/4860265
  * 
+ * A MetaLabeler uses two multi-class classifiers to create a single multi-label classifier. One is called "ranker" and the other is called "counter".
+ * 
+ * The MetaLabeler assigns labels to a sample in the following two stages:
+ *  - Stage 1: Ranking. The sample is sent to the "ranker", which returns all available labels ordered from the most relevant to the least relevant.
+ *  - Stage 2: Counting. The sample is sent to the "counter", which returns integer C >= 0 which represents a number of labels.
+ * The MetaLabeler returns the C most relevant labels from the list returned by the ranker.   
+ * 
  * @param opts
  *            rankerType (mandatory) - the type of the multi-class classifier used for ranking the labels. 
  *            counterType (mandatory) - the type of the multi-class classifier used for selecting the number of labels. 
@@ -31,11 +38,15 @@ MetaLabeler.prototype = {
 	 * Tell the classifier that the given sample belongs to the given classes.
 	 * 
 	 * @param sample  a document.
-	 * @param classes an array whose VALUES are classes.
+	 * @param labels an array whose VALUES are classes.
 	 */
-	trainOnline: function(sample, classes) {
-		this.ranker.trainOnline(sample, classes);
-		this.counter.trainOnline(sample, (Array.isArray(classes)? classes: Object.keys(classes)).length);
+	trainOnline: function(sample, labels) {
+		// The ranker is just trained by the given set of relevant labels:
+		this.ranker.trainOnline(sample, labels);
+
+		// The counter is trained by the *number* of relevant labels:
+		var labelCount = (Array.isArray(labels)? labels: Object.keys(labels)).length;
+		this.counter.trainOnline(sample, labelCount);
 	},
 
 	/**
@@ -46,16 +57,18 @@ MetaLabeler.prototype = {
 	 *            {input: sample1, output: [class11, class12...]}
 	 */
 	trainBatch : function(dataset) {
+		// The ranker is just trained by the given set of labels relevant to each sample:
 		this.ranker.trainBatch(dataset);
-		//console.dir(dataset);
-		var countDataset = dataset.map(function(datum) {
+
+		// The counter is trained by the *number* of labels relevant to each sample:
+		var labelCountDataset = dataset.map(function(datum) {
+			var labelCount = (Array.isArray(datum.output)? datum.output.length: 1);
 			return {
 				input: datum.input,
-				output: (Array.isArray(datum.output)? datum.output.length: 1)
+				output: labelCount
 			};
 		});
-		//console.dir(countDataset);
-		this.counter.trainBatch(countDataset);
+		this.counter.trainBatch(labelCountDataset);
 	},
 
 	/**
@@ -70,11 +83,12 @@ MetaLabeler.prototype = {
 		var rankedLabelsWithExplain = this.ranker.classify(sample, explain, /*withScores=*/true);
 		var rankedLabels = (explain>0? rankedLabelsWithExplain.classes: rankedLabelsWithExplain);
 		var labelCountWithExplain = this.counter.classify(sample, explain, /*withScores=*/true);
-		//console.dir(labelCountWithExplain);
 		var labelCount = (explain>0? labelCountWithExplain.classes[0][0]: labelCountWithExplain[0][0]);
 		if (_.isString(labelCount)) labelCount = parseInt(labelCount);
-		//console.log("labelCount="+labelCount);
+		
+		// Pick the labelCount most relevant labels from the list returned by the ranker:   
 		var positiveLabelsWithScores = rankedLabels.slice(0, labelCount);
+
 		var positiveLabels = positiveLabelsWithScores.map(function(labelWithScore) {return labelWithScore[0]});
 		return (explain>0? {
 			classes: positiveLabels,

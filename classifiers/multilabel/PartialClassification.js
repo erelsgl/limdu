56d502945344b586fed344b0da86fc7d31389842
vasily <elgesto@gmail.com>
2014-02-27 14:56:49 +0200

    done

--- classifiers/multilabel/PartialClassification.js
+++ classifiers/multilabel/PartialClassification.js
@@ -1,44 +1,17 @@
-var hash = require("../../utils/hash");
 var sprintf = require("sprintf").sprintf;
 var _ = require("underscore")._;
-var fs = require('fs');
 var partitions = require('../../utils/partitions');
-var execSync = require('execSync')
-var crypto = require('crypto')
-var execSync = require('execSync').exec
 var multilabelutils = require('./multilabelutils');
 
-
 /**
- * Adaptive Boosting (Adaboost) is a greedy search for a linear combination of 
- * classifiers by overweighting the examples that are misclassified by each 
- * classifier. icsiboost implements Adaboost over stumps (one-level decision trees) 
- * on discrete and continuous attributes (words and real values). 
- * See http://en.wikipedia.org/wiki/AdaBoost and the papers by Y. Freund and R. Schapire for more details.
- * 
- * @param opts
- *            ngram_length (optional) 
- *            iterations (optional) 
- *  
- * The class uses icsiboost open-source implementation of Boostexter
- * https://code.google.com/p/icsiboost/
+ *  PartialClassification is a test classifier that learns and classifies the components \
+ * of the labels separately, for example, a labels consist of 3 part: intent, attribute, value
+ * so there are a classifier for intent, attribute and value.
+ *
  */
 
 var PartialClassification = function(opts) {
-	// if (!Adaboost.isInstalled()) {
-	// 	var msg = "Cannot find the executable 'icsiboost'.";
-	// 	console.error(msg)
-	// 	throw new Error(msg); 
-	// }
-
-	// this.set_of_labels = []
-	// this.text_expert = 'ngram'
-	// this.assigner = crypto.randomBytes(20).toString('hex');
-	// this.folder = "icsiboost_data"
-
-	// this.ngram_length = opts.ngram_length || 2
-	// this.iterations = opts.iterations || 2000
-
+	
 	opts = opts || {};
 	if (!opts.multilabelClassifierType) {
 		console.dir(opts);
@@ -46,30 +19,21 @@ var PartialClassification = function(opts) {
 	}
 	this.multilabelClassifierType = opts.multilabelClassifierType;
 	this.splitLabel = opts.splitLabel || function(label)      {return label.split(/@/);}
-	this.joinLabel  = opts.joinLabel  || function(superlabel) {return superlabel.join("@");}
-	// this.classifier1 =  new this.multilabelClassifierType();
-	// this.classifier2 =  new this.multilabelClassifierType();
-	// this.classifier3 =  new this.multilabelClassifierType();
 	this.classifier = []
-	this.allClasses = {}
 }
 
 
 PartialClassification.prototype = {
 
 	trainOnline: function(sample, labels) {
-	
+
 	},
 
 	trainBatch : function(dataset) {
-		
 		classifier = new this.multilabelClassifierType();
-
 		dataset = this.transformdataset(dataset)
-		
 		classifier.trainBatch(dataset)
 		this.classifier.push(classifier)
-
 	},
 
 	classify: function(sample, explain) {
@@ -98,7 +62,6 @@ PartialClassification.prototype = {
 	},
 
 	transformdataset: function(dataset) {
-		
 		dataset = dataset.map(function(datum) {
 		return {
 				input: datum.input,
@@ -125,7 +88,6 @@ PartialClassification.prototype = {
 	
 	},
 }
-
 
 module.exports = PartialClassification;
 

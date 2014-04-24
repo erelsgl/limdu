f5d548821b40fd537debff278829789062625f8b
vasily <elgesto@gmail.com>
2014-04-24 20:07:05 +0300

    Option for not inserting [begin] [end] in feature generation routine

--- features/NGramsFromArray.js
+++ features/NGramsFromArray.js
@@ -1,16 +1,19 @@
 /**
  * Convert an array of words/tokens to a set of n-grams, for a given n, possibly with a gap:
  */
-module.exports = function(numOfWords, gap, grams, features) {
+module.exports = function(numOfWords, gap, grams, features, nobegin) {
 				for (var i=0; i<numOfWords-1-(gap?1:0); ++i) {
-					grams.unshift("[start]");
-					grams.push("[end]");
+					if (!(nobegin))
+						{
+						grams.unshift("[start]");
+						grams.push("[end]");
+						}
 				}
 				for (var i=0; i<=grams.length-numOfWords; ++i) {
 					sliceOfWords = grams.slice(i, i+numOfWords);
 					if (gap) sliceOfWords[1]="-";
 					var feature = sliceOfWords.join(" ");
-					features[feature]=1;
+					features[feature.trim()]=1;
 				}
 				for (var i=0; i<numOfWords-1-(gap?1:0); ++i) {
 					grams.pop();

798b73c44c1d6a88a164461e2867af022476d520
vasily <elgesto@gmail.com>
2014-05-20 16:01:35 +0300

    test for list embeding

--- test/utilsTest/ListTest.js
+++ test/utilsTest/ListTest.js
@@ -1,5 +1,6 @@
 var ulist = require('../../utils/list');
 var should = require('should');
+var _ = require('underscore');
 
 describe('List test function', function() {
 
@@ -17,6 +18,15 @@ describe('List test function', function() {
 	it('it should calculate median correctly', function() {
 		var list1 = [3, 8, 9, 1, 5, 7, 9, 21];
 		ulist.median(list1).should.be.equal(7.5)
+	})
+
+	it('it should know how to do embedding', function() {
+		_.isEqual(ulist.listembed(['label']), [['label']]).should.equal(true)
+		_.isEqual(ulist.listembed([]), [[]]).should.equal(true)
+		_.isEqual(ulist.listembed(undefined), [[]]).should.equal(true)
+		_.isEqual(ulist.listembed(null), [[]]).should.equal(true)
+		_.isEqual(ulist.listembed({'classes':'label'}), ['label']).should.equal(true)
+		_.isEqual(ulist.listembed({'classes':['label']}),[['label']]).should.equal(true)
 	})
 
 })

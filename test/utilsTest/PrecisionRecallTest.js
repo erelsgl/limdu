c81b8956180773d8073949aa8e887cb42bfe1e7c
vasily <elgesto@gmail.com>
2014-05-25 21:13:35 +0300

    Add test for precisionrecall

--- test/utilsTest/PrecisionRecallTest.js
+++ test/utilsTest/PrecisionRecallTest.js
@@ -10,12 +10,9 @@ var mlutils = require('../../utils');
 var _ = require('underscore');
 
 describe('PrecisionRecall object', function() {
-	var pr = new mlutils.PrecisionRecall();
 	it('correctly calculates precision, recall, etc.', function() {
-		var expectedClasses = [1,2,3,4,5];
-		var actualClasses   = [1,3,5,7];
-		pr.addCases(expectedClasses, actualClasses);
-		
+		var pr = new mlutils.PrecisionRecall();
+		pr.addCases([1,2,3,4,5] , [1,3,5,7]);
 		pr.calculateStats();
 		pr.Accuracy.should.equal(0);
 		pr.count.should.equal(1);
@@ -27,17 +24,35 @@ describe('PrecisionRecall object', function() {
 	});
 
 	it('correctly calculates precision, recall, accuracy in hash format.', function() {
-		stats = pr.addCasesHash([1,2], [1,2], 1);
-		stats = pr.addCasesHash([1,2,3,4,5], [1,3,5,7], 1);
+		var pr = new mlutils.PrecisionRecall();
+		var stats = pr.addCasesHash([1,2,3,4,5], [1,3,5,7], 1);
+		_.isEqual(stats['TP'], ['1','3','5']).should.equal(true)
+		_.isEqual(stats['FP'], ['7']).should.equal(true)
+		_.isEqual(stats['FN'], ['2','4']).should.equal(true)
 		
-		_.difference(stats['TP'], ['1','3','5']).length.should.equal(0)
-		_.difference(stats['FP'], ['7','1']).length.should.equal(0)
-		_.difference(stats['FN'], ['2','','4']).length.should.equal(0)
-		
-		results = pr.retrieveStats()
-		
+		var stats = pr.addCasesHash([1,2], [2,1], 1);
+		var stats = pr.addCasesHash([1,2], [2,1,5], 1);
+
+		var results = pr.retrieveStats()
+
 		results['Accuracy'].should.equal(1/3)
-		results['Precision'].should.equal(0.8)
-		results['Recall'].should.equal(2/3)
+		results['Precision'].should.equal(7/9)
+		results['Recall'].should.equal(7/9)
 	});
+
+	it('correctly calculates precision, recall, accuracy in hash format.', function() {
+		var pr = new mlutils.PrecisionRecall();
+		var stats = pr.addCasesLabels([1,2,3,4,5], [1,3,5,7], 1);
+		_.isEqual(pr.labels['1'],{ TP: 1, FP: 0, FN: 0 }).should.equal(true)
+		var stats = pr.addCasesLabels([1,5,9,2], [7,4], 1);
+		_.isEqual(pr.labels['1'],{ TP: 1, FP: 0, FN: 1 }).should.equal(true)
+		_.isEqual(pr.labels['7'],{ TP: 0, FP: 2, FN: 0 }).should.equal(true)
+		_.isEqual(pr.labels['9'],{ TP: 0, FP: 0, FN: 1 }).should.equal(true)
+		var stats = pr.addCasesLabels([19,4,7], [12,7,4,2,76,19,1], 1);
+		_.isEqual(pr.labels['7'],{ TP: 1, FP: 2, FN: 0 }).should.equal(true)
+		var labels = pr.retrieveLabels()
+		labels['1']['F1'].should.equal(2*0.5*0.5/(1))
+		labels['7']['F1'].should.equal((2*1/3)/(4/3))
+	})
+
 })

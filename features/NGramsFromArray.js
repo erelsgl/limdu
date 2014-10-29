9fc38e84aff420f05372c36d4972e44d349ee28b
vasily <elgesto@gmail.com>
2014-10-29 10:03:55 +0200

    NGramsFromArray.js useless nobegin

--- features/NGramsFromArray.js
+++ features/NGramsFromArray.js
@@ -1,7 +1,7 @@
 /**
  * Convert an array of words/tokens to a set of n-grams, for a given n, possibly with a gap:
  */
-module.exports = function(numOfWords, gap, grams, features, nobegin) {
+module.exports = function(numOfWords, gap, grams, features) {
 				for (var i=0; i<numOfWords-1-(gap?1:0); ++i) {
 					grams.unshift("[start]");
 					grams.push("[end]");

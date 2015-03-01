f68797cb1eec4f129e9f9c448ef3ca25918bdfd4
vasily <elgesto@gmail.com>
2015-03-01 11:55:53 +0200

    Add kNN classifier

--- classifiers/multilabel/index.js
+++ classifiers/multilabel/index.js
@@ -8,6 +8,7 @@ module.exports = {
 	CrossLanguageModel:     require('./CrossLangaugeModelClassifier'),
 	ThresholdClassifier:    require('./ThresholdClassifier'),
 	Adaboost:  				require('./Adaboost'),
+	kNN:  				require('./kNN'),
 	PartialClassification:  require('./PartialClassification'),
 }
 

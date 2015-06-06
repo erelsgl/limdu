12dd841e740e7677afcb75100b2fd1ce9f761cfc
vasily <elgesto@gmail.com>
2015-06-06 19:31:05 +0300

    Allow arrays

--- features/CollectionOfExtractors.js
+++ features/CollectionOfExtractors.js
@@ -8,10 +8,25 @@
  * @param features an initial hash of features (optional).
  * @return a hash with all features generated from the sample by the different extractors
  */
+// module.exports = function(extractors) {
+// 	return function(sample, features) {
+// 		for (var i=0; i<extractors.length; ++i)
+// 			extractors[i](sample, features);
+// 	}
+// }
+
+
+var async = require('async');
+
 module.exports = function(extractors) {
-	return function(sample, features) {
-		for (var i=0; i<extractors.length; ++i)
-			extractors[i](sample, features);
-	}
+	return function(sample, features, stopwords, callback){
+		async.eachSeries(extractors, function(extractor, callback1){ 
+        	extractor(sample, features, stopwords, function(err, result){
+            	callback1()
+            })
+        }, function(err){
+       		callback()
+        })
+    }
 }
 

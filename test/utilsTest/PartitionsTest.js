6eeb4f95cebd4cc03d7d59ca74f36ad528f62988
vasily <elgesto@gmail.com>
2015-10-15 15:07:25 +0300

    Add more tests

--- test/utilsTest/PartitionsTest.js
+++ test/utilsTest/PartitionsTest.js
@@ -10,6 +10,31 @@ var mlutils = require('../../utils');
 var _ = require('underscore');
 
 describe('partitions', function() {
+
+	it('partitions_consistent_by_fold', function() {
+
+		var dataset = [1,3,5,7,9,11,13]
+		var data = mlutils.partitions.partitions_consistent_by_fold(dataset, 2, 1) 
+		_.isEqual(data, { train: [ 1, 3, 5, 13 ], test: [ 7, 9, 11 ] }).should.be.true
+	})
+
+
+	it('partitions_hash_fold', function() {
+		var dataset = {'label1':[1,3,5,7,9,11,13], 'label2':[0,2,4,6,8,10,12]}
+		var data = mlutils.partitions.partitions_hash_fold(dataset, 2, 1) 
+		_.isEqual(data["test"], [7,9,11,6,8,10]).should.be.true		
+		var data = mlutils.partitions.partitions_hash_fold(dataset, 3, 2) 
+		_.isEqual(data["test"], [9,11,8,10]).should.be.true
+	})
+
+	it('partition hash', function() {
+		var dataset = {'label1':[1,3,5,7,9,11,13], 'label2':[0,2,4,6,8,10,12]}
+		mlutils.partitions.partitions_hash(dataset, 2, function(train, test, index) {
+			test.should.have.lengthOf(6);
+			train.should.have.lengthOf(4);
+		})
+	})
+	
 	// A dummy dataset with 10 documents:
 	var dataset = [1,2,3,4,5,6,7,8,9,10];
 	it('creates 5 partitions, with a test-set of 2 in each', function() {

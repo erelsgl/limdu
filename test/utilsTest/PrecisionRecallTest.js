5674be9cffc4a6e13b0d8d4bb4270924dda533d8
vasily <elgesto@gmail.com>
2015-10-23 22:00:19 +0300

    Change PrecisionRecall tests

--- test/utilsTest/PrecisionRecallTest.js
+++ test/utilsTest/PrecisionRecallTest.js
@@ -10,17 +10,60 @@ var mlutils = require('../../utils');
 var _ = require('underscore');
 
 describe('PrecisionRecall object', function() {
+	
+	it('empty sample', function() {
+		var pr = new mlutils.PrecisionRecall();
+		var expl = pr.addCasesHash([], [], true)
+		_.isEqual(expl, {"TP":[]})
+		_.keys(pr.labels).length.should.equal(0)
+	})
+
 	it('correctly calculates precision, recall, etc.', function() {
 		var pr = new mlutils.PrecisionRecall();
-		pr.addCases([1,2,3,4,5] , [1,3,5,7]);
+		var expl = pr.addCasesHash([1,2,3,4,5], [1,3,5,7], true)
+
+		_.isEqual(expl.TP, ["1","3","5"]).should.be.true
+		_.isEqual(expl.FP, ["7"]).should.be.true
+		_.isEqual(expl.FN, ["2","4"]).should.be.true
+
+		pr.labels["1"]["TP"].should.equal(1)
+		pr.labels["4"]["FN"].should.equal(1)
+		pr.labels["7"]["FP"].should.equal(1)
+		
+		var expl = pr.addCasesHash([1,3,4,5], [1,2,8],true)
+		
+		_.isEqual(expl.TP, ["1"]).should.be.true
+		_.isEqual(expl.FP, ["2","8"]).should.be.true
+		_.isEqual(expl.FN, ["3","4","5"]).should.be.true
+
+		pr.labels["1"]["TP"].should.equal(2)
+		pr.labels["8"]["FP"].should.equal(1)
+		
 		pr.calculateStats();
+
+		pr.labels["1"]["F1"].should.equal(1)
+		pr.labels["3"]["Recall"].should.equal(1/2)
+		pr.labels["3"]["Precision"].should.equal(1)
+		pr.labels["3"]["Recall"].should.equal(0.5)
+
 		pr.Accuracy.should.equal(0);
-		pr.count.should.equal(1);
-		pr.Precision.should.equal(3/4);
-		pr.Recall.should.equal(3/5);
-		pr.F1.should.equal(2/(4/3+5/3));
-		pr.HammingLoss.should.equal(3/5);
-		pr.HammingGain.should.equal(2/5);
+		pr.microPrecision.should.equal(4/(4+3));
+		pr.microRecall.should.equal(4/(4+5));
+		pr.microF1.should.equal(2*pr.microPrecision*pr.microRecall/(pr.microPrecision+pr.microRecall));
+
+		pr.macroRecall.should.equal((0.5 + 0 + 0.5 + 0 + 1)/5)
+		pr.macroPrecision.should.equal(3/6)
+		pr.macroF1.should.equal((1+2/3+2/3)/3)
+
+		var expl = pr.addCasesHash([5,2,7,8], [2,3,7,4,8],true)
+
+		pr.labels["2"]["TP"].should.equal(1)
+		
+		pr.calculateStats();
+
+		pr.macroF1.should.equal((2/3+2/3+0.5+0.5+0.5+1)/6)
+		pr.macroPrecision.should.equal((1+0.5+0.5+0+1+0.5+0.5)/7)
+		pr.macroRecall.should.equal((1+0.5+0.5+0+1/3+1+1)/7)
 	});
 
 	it('correctly calculates precision, recall, accuracy in hash format.', function() {
@@ -33,126 +76,126 @@ describe('PrecisionRecall object', function() {
 		var stats = pr.addCasesHash([1,2], [2,1], 1);
 		var stats = pr.addCasesHash([1,2], [2,1,5], 1);
 
-		var results = pr.retrieveStats()
+		pr.calculateStats()
 
-		results['Accuracy'].should.equal(1/3)
-		results['Precision'].should.equal(7/9)
-		results['Recall'].should.equal(7/9)
+		pr.Accuracy.should.equal(1/3)
+		pr.microPrecision.should.equal(7/9)
+		pr.microRecall.should.equal(7/9)
 	});
 
-	it('uniqueaggregate', function() {
-		var pr = new mlutils.PrecisionRecall();
-		var ac = [['Greet',[1,5]], ['Offer',[0,5]], ['Greet',[0,6]], ['Offer',[9,15]], ['Greet',[0,9]]]
-		var output = pr.uniqueaggregate(ac)
-		output.length.should.equal(3)
+	// it.skip('uniqueaggregate', function() {
+	// 	var pr = new mlutils.PrecisionRecall();
+	// 	var ac = [['Greet',[1,5]], ['Offer',[0,5]], ['Greet',[0,6]], ['Offer',[9,15]], ['Greet',[0,9]]]
+	// 	var output = pr.uniqueaggregate(ac)
+	// 	output.length.should.equal(3)
 
-		var actual = pr.uniquecandidate(output)
-		_.isEqual(actual[0], ['Greet',[0,9]]).should.be.true
-	})	
+	// 	var actual = pr.uniquecandidate(output)
+	// 	_.isEqual(actual[0], ['Greet',[0,9]]).should.be.true
+	// })	
 
-	it('correctly calculates precision, recall, accuracy in sequence format.', function() {
-        var pr = new mlutils.PrecisionRecall();
+	// it.skip('correctly calculates precision, recall, accuracy in sequence format.', function() {
+ //        var pr = new mlutils.PrecisionRecall();
 		
-		var expected =  
-				[
-                	["Offer", [13,26]],
-                	["Working Hours", [6,9]],
-                	["8 hours", [3,12]],
-                	["10 hours", []]
-				]
+	// 	var expected =  
+	// 			[
+ //                	["Offer", [13,26]],
+ //                	["Working Hours", [6,9]],
+ //                	["8 hours", [3,12]],
+ //                	["10 hours", []]
+	// 			]
 
-		var actual = 
-				[
-					['Offer',[13,25]],
-					['Salary', [3,6]],
-					['8 hours', [3,5]],
-					['10 hours', [9,15]],
-				]
+	// 	var actual = 
+	// 			[
+	// 				['Offer',[13,25]],
+	// 				['Salary', [3,6]],
+	// 				['8 hours', [3,5]],
+	// 				['10 hours', [9,15]],
+	// 			]
 			
-        var stats = pr.addCasesHashSeq(expected, actual, 1);
+ //        var stats = pr.addCasesHashSeq(expected, actual, 1);
 
-        _.isEqual(stats['TP'], ['10 hours', '8 hours', 'Offer']).should.equal(true)
-        _.isEqual(stats['FP'], ['Salary']).should.equal(true)
-        _.isEqual(stats['FN'], ['Working Hours']).should.equal(true)
+ //        _.isEqual(stats['TP'], ['10 hours', '8 hours', 'Offer']).should.equal(true)
+ //        _.isEqual(stats['FP'], ['Salary']).should.equal(true)
+ //        _.isEqual(stats['FN'], ['Working Hours']).should.equal(true)
 
-        var results = pr.retrieveStats()
+ //        var results = pr.retrieveStats()
 
-        var stats = pr.addCasesHashSeq(expected, actual, 1);
-    });
+ //        var stats = pr.addCasesHashSeq(expected, actual, 1);
+ //    });
 
-    it('intersection', function() {
-        var pr = new mlutils.PrecisionRecall();
+  //   it.skip('intersection', function() {
+  //       var pr = new mlutils.PrecisionRecall();
     	
-    	var actual = [5,10]
-    	var expected = [8,15]
+  //   	var actual = [5,10]
+  //   	var expected = [8,15]
 
-    	var stats = pr.intersection(actual, expected)
-    	stats.should.be.true
+  //   	var stats = pr.intersection(actual, expected)
+  //   	stats.should.be.true
 
-    	var actual = [5,10]
-    	var expected = [12,15]
+  //   	var actual = [5,10]
+  //   	var expected = [12,15]
 		
-		var stats = pr.intersection(actual, expected)
-    	stats.should.be.false
-    })
+		// var stats = pr.intersection(actual, expected)
+  //   	stats.should.be.false
+  //   })
 
-    it('repetitions in sequence format', function() {
-    	var pr = new mlutils.PrecisionRecall();
-    	var expected =  [[]]
-    	var actual = 
-				[
-					['Offer',[13,25], 'i offer'],
-					['Offer', [14,27], 'ok'],
-				]
+  //   it.skip('repetitions in sequence format', function() {
+  //   	var pr = new mlutils.PrecisionRecall();
+  //   	var expected =  [[]]
+  //   	var actual = 
+		// 		[
+		// 			['Offer',[13,25], 'i offer'],
+		// 			['Offer', [14,27], 'ok'],
+		// 		]
 		
-		var stats = pr.addCasesHashSeq(expected, actual, 1);
-		_.isEqual(stats, { TP: [], FP: [ 'Offer' ], FN: [] }).should.be.true
-    })
+		// var stats = pr.addCasesHashSeq(expected, actual, 1);
+		// _.isEqual(stats, { TP: [], FP: [ 'Offer' ], FN: [] }).should.be.true
+  //   })
 
 	it('correctly calculates precision, recall, accuracy in hash format.', function() {
 		var pr = new mlutils.PrecisionRecall();
-		var stats = pr.addCasesLabels([1,2,3,4,5], [1,3,5,7], 1);
+		var stats = pr.addCasesHash([1,2,3,4,5], [1,3,5,7], 1);
 		_.isEqual(pr.labels['1'],{ TP: 1, FP: 0, FN: 0 }).should.equal(true)
-		var stats = pr.addCasesLabels([1,5,9,2], [7,4], 1);
+		var stats = pr.addCasesHash([1,5,9,2], [7,4], 1);
 		_.isEqual(pr.labels['1'],{ TP: 1, FP: 0, FN: 1 }).should.equal(true)
 		_.isEqual(pr.labels['7'],{ TP: 0, FP: 2, FN: 0 }).should.equal(true)
 		_.isEqual(pr.labels['9'],{ TP: 0, FP: 0, FN: 1 }).should.equal(true)
-		var stats = pr.addCasesLabels([19,4,7], [12,7,4,2,76,19,1], 1);
+		var stats = pr.addCasesHash([19,4,7], [12,7,4,2,76,19,1], 1);
 		_.isEqual(pr.labels['7'],{ TP: 1, FP: 2, FN: 0 }).should.equal(true)
-		var labels = pr.retrieveLabels()
-		labels['1']['F1'].should.equal(2*0.5*0.5/(1))
-		labels['7']['F1'].should.equal((2*1/3)/(4/3))
-		var stat = pr.calculateStats()
-	})
-
-	it.skip('correctly calculates dependencies between labels', function() {
-
-		var pr = new mlutils.PrecisionRecall();
-    	var expected =  [[]]
-    	var actual = 
-				[
-					['Offer',[13,25], 'i offer'],
-					['Accept', [10,27], 'offer'],
-					['Reject', [30,45], 'reject'],
-				]
-		
-		var stats = pr.addCasesHashSeq(expected, actual, 1);
-
-		var actual = 
-				[
-					['Offer',[13,25], 'i offer'],
-					['Offer',[14,25], 'offer'],
-					['Accept', [60,70], 'agree'],
-					['Reject', [90,100], 'decline']
-				]
-
-		var stats = pr.addCasesHashSeq(expected, actual, 1);
 		pr.calculateStats()
-
-		var gold = {"Offer": { "Offer": [ "i offer", "i offer" ], "Accept": [ "offer" ] },
-        "Accept": { "Accept": [ "offer", "agree" ], "Offer": [ "i offer" ] },
-        "Reject": { "Reject": [ "reject", "decline" ] }}
-
-		_.isEqual(pr.retrieveStats()['interdep'], gold).should.be.true
+		pr.labels['1']['F1'].should.equal(2*0.5*0.5/(1))
+		pr.labels['7']['F1'].should.equal((2*1/3)/(4/3))
 	})
+
+	// it.skip('correctly calculates dependencies between labels', function() {
+
+	// 	var pr = new mlutils.PrecisionRecall();
+ //    	var expected =  [[]]
+ //    	var actual = 
+	// 			[
+	// 				['Offer',[13,25], 'i offer'],
+	// 				['Accept', [10,27], 'offer'],
+	// 				['Reject', [30,45], 'reject'],
+	// 			]
+		
+	// 	var stats = pr.addCasesHashSeq(expected, actual, 1);
+
+	// 	var actual = 
+	// 			[
+	// 				['Offer',[13,25], 'i offer'],
+	// 				['Offer',[14,25], 'offer'],
+	// 				['Accept', [60,70], 'agree'],
+	// 				['Reject', [90,100], 'decline']
+	// 			]
+
+	// 	var stats = pr.addCasesHashSeq(expected, actual, 1);
+	// 	pr.calculateStats()
+
+	// 	var gold = {"Offer": { "Offer": [ "i offer", "i offer" ], "Accept": [ "offer" ] },
+ //        "Accept": { "Accept": [ "offer", "agree" ], "Offer": [ "i offer" ] },
+ //        "Reject": { "Reject": [ "reject", "decline" ] }}
+
+	// 	_.isEqual(pr.retrieveStats()['interdep'], gold).should.be.true
+	// })
 })
+

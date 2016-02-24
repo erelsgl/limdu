a5d91a1c5af936e31f96c8e6864c96f6e1d8dcc3
vasily <elgesto@gmail.com>
2016-02-24 14:22:54 +0200

    Add test with folds

--- classifiers/svm/SvmLinearDemo.js
+++ classifiers/svm/SvmLinearDemo.js
@@ -1,8 +1,9 @@
 // simple demonstration of binary SVM, based on LibLinear
 
 var SvmLinear = require('./SvmLinear');
+var partitions = require(__dirname+'/../../utils/partitions');
 
-var trainSet = [
+var dataset = [
 		{input: [0,0], output: 0},
 		{input: [1,1], output: 0},
 		{input: [0,1], output: 1},
@@ -11,7 +12,6 @@ var trainSet = [
 // the separating line goes through [0,0.5] and [1,1.5]. It is:
 //       0.5+x-y = 0
 // or:   -1-2x+2y = 0
-
 
 var classifier = new SvmLinear(
 	{
@@ -22,7 +22,7 @@ var classifier = new SvmLinear(
 	        multiclass: false
 	}
 );
-classifier.trainBatch(trainSet);
+classifier.trainBatch(dataset);
 
 console.log("simple classification: ");
 console.dir(classifier.classify([0,2]));  // 1
@@ -30,3 +30,15 @@ console.dir(classifier.classify([1,0]));  // 0
 
 console.log("model: ");
 console.dir(classifier.mapLabelToMapFeatureToWeight);   // { '0': -1, '1': -2, '2': 2 }
+
+partitions.partitions(dataset.concat(dataset), 2, function(train, test, index) {
+
+	console.log("fold: "+index)
+	classifier.trainBatch(train)
+		
+	test.forEach(function(instance) {
+		console.dir("Classify instance:")
+		console.dir(instance)
+		console.dir(classifier.classify(instance.input));
+	});
+});

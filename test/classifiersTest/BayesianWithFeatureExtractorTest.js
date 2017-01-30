e6867c39a63a3fec2693992d625e1f3360e457fe
Erel Segal-Halevi cron <erelsgl@gmail.com>
2017-01-30 10:37:35 +0200

    fix bayesian test

--- test/classifiersTest/BayesianWithFeatureExtractorTest.js
+++ test/classifiersTest/BayesianWithFeatureExtractorTest.js
@@ -1,6 +1,7 @@
+#!mocha
 /**
  * a unit-test for Enhanced Classifier
- * 
+ *
  * @author Erel Segal-Halevi
  * @since 2013-08
  */
@@ -16,7 +17,7 @@ var UnigramBayesianClassifier = classifiers.EnhancedClassifier.bind(this, {
 	featureExtractor: ftrs.NGramsOfWords(1),
 });
 
-describe.skip('bayesian classifier with a single feature extractor for words', function() {
+describe('bayesian classifier with a single feature extractor for words', function() {
 	var spamClassifier = new UnigramBayesianClassifier();
 
 	spamClassifier.trainBatch([
@@ -25,7 +26,7 @@ describe.skip('bayesian classifier with a single feature extractor for words', f
 		{input: "I don't know if this works on windows", output: 'nospam'},
 		{input: "cheap windows !!!", output: 'spam'},
 	]);
-	
+
 	it('classifies sentences', function() {
 		spamClassifier.classify("cheap clocks !!!").should.equal('spam')
 		spamClassifier.classify("I don't know if this is a replica of windows").should.equal('nospam');
@@ -33,30 +34,30 @@ describe.skip('bayesian classifier with a single feature extractor for words', f
 		spamClassifier.classify("your").should.equal('nospam');
 		spamClassifier.classify("watch").should.equal('unclassified');
 	})
-	
+
 	it('explains its decisions', function() {
 		var c;
 		c = spamClassifier.classify("cheap clocks !!!",1);
 		c.should.have.property('classes', 'spam');
 		c.should.have.property('explanation');
-		
+
 		c = spamClassifier.classify("I don't know if this is a replica of windows",1);
 		c.should.have.property('classes', 'nospam');
 		c.should.have.property('explanation');
-		
+
 		c = spamClassifier.classify("replica",1);
 		c.should.have.property('classes', 'spam');
 		c.should.have.property('explanation');
-		
+
 		c = spamClassifier.classify("your",1);
 		c.should.have.property('classes', 'nospam');
 		c.should.have.property('explanation');
-		
+
 		c = spamClassifier.classify("watch",1);
 		c.should.have.property('classes', 'unclassified');
 		c.should.have.property('explanation');
 	})
-	
+
 	it.skip('supports continuous output', function() {
 		spamClassifier.classify("I don't know if this is a replica of windows",0,true).should.be.above(0);
 		spamClassifier.classify("replica",0,true).should.be.above(0);
@@ -65,7 +66,7 @@ describe.skip('bayesian classifier with a single feature extractor for words', f
 	});
 })
 
-describe.skip('another bayesian classifier', function() {
+describe('another bayesian classifier', function() {
 	it('classifies sentences', function() {
 		var classifier = new UnigramBayesianClassifier();
 
